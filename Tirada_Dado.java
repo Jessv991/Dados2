075419654cfdedb58bcce23ac2ab23edb0f0385a
Jessv991 <122239589+Jessv991@users.noreply.github.com>
2023-01-27 19:00:44 +0100

    primer commit

---
+++ Tirada_Dado.java
@@ -0,0 +1,24 @@
+
+public class Tirada_Dado {
+
+	public static void main(String[] args) {
+		Tirada();
+
+	}
+
+	private static void Tirada() {
+		int dau1= (int) (Math.random()*6)+1;
+		int dau2= (int) (Math.random()*6)+1;
+		
+		System.out.println(dau1+" "+dau2);
+		
+		if(dau1==dau2) {
+			System.out.println("Ganaste");
+		}
+		else {
+			Tirada();
+		}
+		
+	}
+
+}
