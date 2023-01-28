2d5feecc1305e5989f32fff30d27c1b53620fc5b
Jessv991 <122239589+Jessv991@users.noreply.github.com>
2023-01-28 03:02:55 +0100

    primer commit de NuevaRama

---
+++ src/Persona.java
@@ -0,0 +1,8 @@
+
+public class Persona {
+
+	    private int edat;
+	    Persona() {edat=0;}
+	    public int aniversari(int e){e++; return e;}
+	}
+	
---
+++ src/Test2.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Test2 {
+	@Test
+	public void test() {
+	
+
+	        Persona p = new Persona();
+	        assertEquals(6,p.aniversari(5));
+	}
+
+	
+			
+	}
+
